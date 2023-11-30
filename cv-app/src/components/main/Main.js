24303ab2292941ca4e6e65a6c8bfc6beb2621ab2
Ella Gryf-Lowczowska <ellagryflow@gmail.com>
2023-11-30 18:25:18 +0000

    Navbar implemented and styled responsively

--- cv-app/src/components/main/Main.js
+++ cv-app/src/components/main/Main.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <main className='base-grid'>
+      Main
+    </main>
   )
 }
 
--- cv-app/src/components/navbar/Navbar.js
+++ cv-app/src/components/navbar/Navbar.js
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import {HiOutlineMenuAlt4} from 'react-icons/hi';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { RxHamburgerMenu } from "react-icons/rx";
 import {FaRegTimesCircle} from 'react-icons/fa';
 import "./Navbar.css";
+
+const socials = [
+  {
+    icon: faEnvelope,
+    url: "mailto: ellagryflow@gmail.com",
+  },
+  {
+    icon: faGithub,
+    url: "https://github.com/Ella-Gryf/curriculum-vitae.git",
+  },
+  {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com/in/ella-gryf-lowczowska/", 
+  },
+];
 
 const Navbar = () => {
 
@@ -19,35 +37,45 @@ const Navbar = () => {
   }
 
   return (
-    <header className="navbar">
-      <h3>icons - here</h3>
-      <nav role="navigation" >
-        <li>
-          <Link to="/" className={isActive(loc, "/")}>Home</Link>
-        </li>
-        <li>
-          <Link to="/about" className={isActive(loc, "/about")}>About</Link>
-        </li>
-        <li>
-          <Link to="/software" className={isActive(loc, "/software")}>Software</Link>
-        </li>
-        <li>
-          <Link to="/education" className={isActive(loc, "/education")}>Education</Link>
-        </li>
-        <li>
-          <Link to="/volunteering" className={isActive(loc, "/volunteering")}>Volunteering</Link>
-        </li>
-        <li>
-          <Link to="references" className={isActive(loc, "/references")}>References</Link>
-        </li>
-      </nav>
-      <div 
-        className="hamburger" 
-        role="button" 
-        aria-label="Toggle Navigation Menu"
-        onClick={handleClick}
-      >
-        {click ? (<FaRegTimesCircle className='icon' />) : (<HiOutlineMenuAlt4 className='icon' />)}
+    <header className="navbar base-grid">
+      <div className='base-container'>
+        <nav id="socials">
+          {socials.map(({icon, url}) => (
+            <a key={url} href={url} icon={icon}>
+              <FontAwesomeIcon icon={icon} size="2x" />
+            </a>
+          ))}
+        </nav>
+        <nav role="navigation" id="site-links">
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li>
+              <Link to="/" className={`${isActive(loc, "/")} li`}>Home</Link>
+            </li>
+            <li>
+              <Link to="/about" className={`${isActive(loc, "/about")} li`}>About</Link>
+            </li>
+            <li>
+              <Link to="/software" className={`${isActive(loc, "/software")} li`}>Software</Link>
+            </li>
+            <li>
+              <Link to="/education" className={`${isActive(loc, "/education")} li`}>Education</Link>
+            </li>
+            <li>
+              <Link to="/volunteering" className={`${isActive(loc, "/volunteering")} li`}>Volunteering</Link>
+            </li>
+            <li>
+              <Link to="references" className={`${isActive(loc, "/references")} li`}>References</Link>
+            </li>
+          </ul>
+        </nav>
+        <div 
+          className="hamburger" 
+          role="button" 
+          aria-label="Toggle Navigation Menu"
+          onClick={handleClick}
+        >
+          {click ? (<FaRegTimesCircle className='icon' />) : (<RxHamburgerMenu className='icon' />)}
+        </div>
       </div>
     </header>
   )
