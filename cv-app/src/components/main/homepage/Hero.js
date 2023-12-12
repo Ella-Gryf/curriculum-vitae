afbd53031ce329ad8a71ab1715354a1a702b38bf
Ella Gryf-Lowczowska <ellagryflow@gmail.com>
2023-12-12 10:47:33 +0000

    Hero backup background path

--- cv-app/src/components/main/homepage/Hero.js
+++ cv-app/src/components/main/homepage/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className='homepage hero'>
 
     <div className="fullscreen-video-wrap" dangerouslySetInnerHTML={{ __html: `
-      <video playsinline loop autoPlay muted autobuffer poster="../../../assets/freepik/backuphero.jpg">
+      <video playsinline loop autoPlay muted autobuffer poster=${require('../../../assets/freepik/backuphero.jpg')}>
         <source src="${require('../../../assets/freepik/microchip.mp4')}" type="video/mp4" />
         <img src="${require('../../../assets/freepik/backuphero.jpg')}" alt=""/>
       </video>            
